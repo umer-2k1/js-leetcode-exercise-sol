035b4a475546e1868309391760642b0be2cbe0ae
umermemon4648 <umermemon4648@gmail.com>
2023-04-16 20:11:03 +0500

    optimized solution

--- twoSumArray.js
+++ twoSumArray.js
@@ -28,9 +28,24 @@ var twoSum = function(nums, target) {
         }
     }
 };
+  
+// Optimized solution having time complexity O(n)
+const twoSum = (nums, target) => {
+    const map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+      const complement = target - nums[i];
+      if (map.has(complement)) {
+        return [map.get(complement), i];
+      }
+      map.set(nums[i], i);
+    }
+  };
+  
 
 // let nums = [2,7,11,15]
 // let target = 9
-let nums = [3,2,4]
-let target = 6
-twoSum(nums,target)
+let nums = [3,2,4,3,2,6,8]
+let target = 12
+
+
+console.log(twoSum(nums,target))
