679501ba0792ecc3dfe1024f6595e12c5a3e71bb
umermemon4648 <umermemon4648@gmail.com>
2023-05-25 21:29:42 +0500

    truncate sentences

---
+++ truncateSentence.js
@@ -0,0 +1,53 @@
+
+// https://leetcode.com/problems/truncate-sentence/
+/*
+
+A sentence is a list of words that are separated by a single space with no leading or trailing spaces. Each of the words consists of only uppercase and lowercase English letters (no punctuation).
+
+For example, "Hello World", "HELLO", and "hello world hello world" are all sentences.
+You are given a sentence s​​​​​​ and an integer k​​​​​​. You want to truncate s​​​​​​ such that it contains only the first k​​​​​​ words. Return s​​​​​​ after truncating it.
+
+ 
+
+Example 1:
+
+Input: s = "Hello how are you Contestant", k = 4
+Output: "Hello how are you"
+Explanation:
+The words in s are ["Hello", "how" "are", "you", "Contestant"].
+The first 4 words are ["Hello", "how", "are", "you"].
+Hence, you should return "Hello how are you".
+Example 2:
+
+Input: s = "What is the solution to this problem", k = 4
+Output: "What is the solution"
+Explanation:
+The words in s are ["What", "is" "the", "solution", "to", "this", "problem"].
+The first 4 words are ["What", "is", "the", "solution"].
+Hence, you should return "What is the solution".
+Example 3:
+
+Input: s = "chopper is not a tanuki", k = 5
+Output: "chopper is not a tanuki"
+
+*/
+
+
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var truncateSentence = function(s, k) {
+    // return s.split(" ").slice(0,k).join(" ")
+    let words=s.split(" ")
+    let newStr = ""
+    for (let i = 0; i < k; i++) {
+      newStr = newStr.concat(words[i], " ")
+        
+    }
+    return newStr
+};
+let s = "What is the solution to this problem"
+let k = 4
+console.log(truncateSentence(s,k))
